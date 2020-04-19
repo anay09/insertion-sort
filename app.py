39adf687c14ad8cb54b5e4d8df331f526eff6d4a
anayc <anay.chopade>
2020-04-19 15:48:51 +0100

    implemented the insertion sort algorithm into the python file Entire program functions properly

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ while no_of_numbers > 20:
     no_of_numbers = int(input("How many numbers will you enter (max:20) : "))
 
 num_arr = {}
+temp = 0
+
 for i in range(no_of_numbers):
     num_arr[i] = int(input("Enter a number : "))
+
+for i in range(no_of_numbers):
+    for j in range(no_of_numbers):
+        if num_arr[i] < num_arr[j]:
+            if i > j:
+                temp = num_arr[i]
+                num_arr[i] = num_arr[j]
+                num_arr[j] = temp
+
+for i in range(no_of_numbers):
+    print(num_arr[i])
 
